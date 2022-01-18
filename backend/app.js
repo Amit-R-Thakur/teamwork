190bb533215ca8bebe561701a37c67d94f2ecfb7
anshika056 <anshikamadan2@gmail.com>
2022-01-18 16:19:00 +0530

    added the user signup routes and control

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,23 @@
 const express=require("express")
 const app=express()
+const dotenv = require("dotenv");
 const port=process.env.PORT||6000
+dotenv.config({path:"./config.env"});
 require("./database/connect")
-app.get("/",(req,re)=>{
+
+const User = require("../backend/models/user");
+
+const userroutes = require("./routes/user");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.use("/api",userroutes);
+
+
+app.get("/",(req,res)=>{
     res.send("hello")
 })
 app.listen(port,()=>{
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,8 @@
 const mongoose =  require("mongoose")
 const validator =  require("validator")
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+require("dotenv").config();
 const userSchema = mongoose.Schema({
     name:{
         type:String,
@@ -22,6 +25,17 @@ const userSchema = mongoose.Schema({
     }
 },{timestamps:true});
 
+userSchema.pre("save",async function(next){
+    console.log("hash")
+    this.password = await bcrypt.hash(this.password,12);
+    next();
+})
+
+userSchema.methods.genToken=async function(){
+    const token = await jwt.sign({_id:this._id},process.env.secret_key);
+    return token
+
+} 
 
 const User = new mongoose.model("USER",userSchema);
 module.exports= User;
