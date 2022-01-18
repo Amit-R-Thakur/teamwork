83e3c6a92c0c801e0f44861e07f4c3a8e48196b2
anshika056 <anshikamadan2@gmail.com>
2022-01-18 20:38:56 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,19 @@ const express=require("express")
 const app=express()
 const port=process.env.PORT||6000
 require("./database/connect")
+
+
+const User = require("./teamwork/backend/models/user");
+
+const userroutes = require("./teamwork/backend/routes/user");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.use("/api",userroutes);
+
 app.get("/request",(req,re)=>{
     res.send("hello")
 })
---
+++ backend/models/user.js
@@ -0,0 +1,41 @@
+const mongoose =  require("mongoose")
+const validator =  require("validator")
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+require("dotenv").config();
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true,
+        validate: (value) => {
+            if (!validator.isEmail(value)) {
+              throw new Error("Invalid Email Address");
+            }
+          }
+    },
+    password:{
+        type:String,
+        required:true
+    }
+},{timestamps:true});
+
+userSchema.pre("save",async function(next){
+    console.log("hash")
+    this.password = await bcrypt.hash(this.password,12);
+    next();
+})
+
+userSchema.methods.genToken=async function(){
+    const token = await jwt.sign({_id:this._id},process.env.secret_key);
+    return token
+
+} 
+
+const User = new mongoose.model("USER",userSchema);
+module.exports= User;
