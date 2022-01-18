ed350f8b71562a585d33c694a85ce1fd0f42730c
anshika056 <anshikamadan2@gmail.com>
2022-01-18 21:25:34 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
-const express=require("express")
+const dotenv = require("dotenv");
+const express = require("express");
 const app=express()
 const port=process.env.PORT||6000
+dotenv.config({path:"./config.env"});
 require("./database/connect")
 
 
-const User = require("./teamwork/backend/models/user");
+const User = require("./models/user");
 
-const userroutes = require("./teamwork/backend/routes/user");
+const userroutes = require("./routes/user");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
