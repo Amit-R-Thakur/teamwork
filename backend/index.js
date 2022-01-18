1fe09e53ae1003786268a57c7c69abd464fbdb7e
Amit_R_Thakur <at913513@gmail.com.com>
2022-01-18 16:59:06 +0530

    problem with hosting

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express=require("express")
 const app=express()
 const port=process.env.PORT||6000
 require("./database/connect")
-app.get("/",(req,re)=>{
+app.get("/request",(req,re)=>{
     res.send("hello")
 })
 app.listen(port,()=>{
