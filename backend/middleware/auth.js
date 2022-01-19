66672e1cdb6a5db8773ec810274e1cdb3f975ca1
anshika056 <anshikamadan2@gmail.com>
2022-01-19 22:22:07 +0530

    added the middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+exports.usermiddleware= async (req,res,next)=>{
+ 
+  try{  
+    const token = req.headers.authorization
+    const validuser = await jwt.verify(token,process.env.secret_key);
+    if(!validuser){
+        res.send("invalid user");
+    }
+    req._id=validuser._id                     
+    next();
+  }
+  catch(err){
+      console.log(err); 
+      res.send("not a valid user");
+  }
+}
--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { createtodo, gettodobyid, updatetodo, deletetodo } = require("../controllers/todocontrol");
 const router = express.Router();
+const {usermiddleware}=require("../middleware/auth");
 
-router.post("/createtodo",createtodo)
-router.get("/gettodo/:id",gettodobyid)
-router.put("/update/:id",updatetodo)
-router.delete("/delete/:id",deletetodo)
+
+router.post("/createtodo",usermiddleware,createtodo)
+router.get("/gettodo/:id",usermiddleware,gettodobyid)
+router.put("/update/:id",usermiddleware,updatetodo)
+router.delete("/delete/:id",usermiddleware,deletetodo)
 module.exports=router;
