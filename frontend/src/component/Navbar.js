161611058f671d753b8c5204cf0edf5dec3cb21b
omkar2120 <naradomkar212000@gmail.com>
2022-01-20 18:13:36 +0530

    todo design done

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -4,14 +4,16 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import Todos from './Todos';
+import { CssBaseline, Typography } from '@mui/material';
 
 
 const Navbar = () => {
 
     return (
         <div>
-             <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+             <Box sx={{ flexGrow: 1}}>
+               
+      <AppBar position="static" style={{height:'70px'}}>
         <Toolbar style={{justifyContent:'flex-end'}}>
          
           <Button color="inherit" >LogOut</Button>
--- frontend/src/component/Todos.js
+++ frontend/src/component/Todos.js
@@ -1,74 +1,113 @@
-import { CssBaseline, Typography , TextField  , Button , FormControlLabel , Checkbox,} from '@mui/material';
+import { CssBaseline , TextField  , Button , Checkbox, } from '@mui/material';
 import React from 'react'
 import Container from '@mui/material/Container'
 import Box from "@mui/material/Box"
-import { display, flexbox, margin } from '@mui/system';
 // import Typography  from '@mui/material/Typography';
-import AddBoxSharpIcon from '@mui/icons-material/AddBoxSharp';
+
 
 const Todos = () => {
 
 
-    
+
 
     return (
         <div >
-            <Container component="main" maxWidth='sm' >
+         <Container component="main" maxWidth='sm'  >
 
-            <Box component='form' maxWidth="sx"  style={{display:'flex'}}>
-            <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="text"
-                    name="text"
-                    autoFocus
-                    autoComplete="off"
-                    placeholder="Add your work here"
-                    
-
-                /> 
-                                <Button style={{
-                                    padding:'10px',
-                                    backgroundColor:'blue' , 
-                                    height:'50px',
-                                    color:'white',
-                                    position:'relative',
-                                    top:'10px',
-                                    margin:'10px'
-                                    }}>Add +</Button>
-
-
-            </Box>
-
-<CssBaseline/>
-            <Box sx={{
-                width:500,
-                alignContent:'center',
-                backgroundColor:'white',
-                
-            }} style={{
-                boxShadow:'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
-                marginTop:20,
-                
-            }}>
-             <Box sx={{display:'flex' , alignItems:'space-around'}}>
-                <Checkbox/>
-                <h1>Hello</h1>
-                <Box sx={{marginLeft:'200px' , marginTop:'23px'}}>
-                <Button style={{backgroundColor:'#22BCE2', color:'white', }}> Edit</Button>
-                <Button style={{backgroundColor:'#f44336', color:'white'}}> Delete</Button>
+            <Box component='form'  style={{display:'flex', marginTop:'15px'}}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="text"
+                        name="text"
+                        autoFocus
+                        autoComplete="off"
+                        placeholder="Add your work here"
+                   /> 
+                    <Button style={{
+                         padding:'10px',
+                         backgroundColor:'blue' , 
+                         height:'50px',
+                         color:'white',
+                         position:'relative',
+                         top:'10px',
+                         margin:'10px'
+                        }}
+                        >Add +
+                     </Button>
                 </Box>
-                
+        </Container>
+
+
+             <Container style={{width:'80%'}} >
+
+               <Box sx={{                                                                     //todos
+                    backgroundColor:'white', marginTop:'15px'
+                       }} 
+                       style={{
+                           boxShadow:'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
+                           
+                             }}>
+ {/* main div todo */}
+
+         <Box style={{display:'flex' , marginTop:'15px'}}>
+                <Box width="15%" style={{marginTop:'15px'}}>  
+                      <Checkbox />
+                </Box>
+
+                <Box style={{width:'60%', margin:'px'}}>
+                    <h4>Hello World</h4>
+                </Box>
+       
+                <Box style={{ margin:'15px' }}>
+                    <Button style={{color:'#22B4DB'}}> Edit</Button>
+                    <Button style={{color:'#DE192E'}}>Delete</Button>
+                 </Box>
+            
+             </Box>
+{/* second div */}
+         <Box style={{display:'flex', marginTop:'15px'}}>
+            <CssBaseline/>
+                <Box width="15%" style={{marginTop:'15px'}}>
+                      <Checkbox/>
+                </Box>
+
+                <Box style={{width:'60%', margin:'px'}}>
+                    <h4>Hello World</h4>
+                </Box>
+
+                <Box style={{ margin:'15px' }}>
+                    <Button style={{color:'#22B4DB'}}> Edit</Button>
+                    <Button style={{color:'#DE192E'}}>Delete</Button>
+                 </Box>
              </Box>
              
-           
-             
-             
-           </Box>
-           
-            </Container>
+{/* second div */}
+
+         <Box style={{display:'flex', marginTop:'15px'}}>
+            <CssBaseline/>
+                <Box width="15%" style={{marginTop:'15px'}}>
+                      <Checkbox/>
+                </Box>
+
+                <Box style={{width:'60%', margin:'px'}}>
+                    <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</h4>
+                </Box>
+
+                <Box style={{ margin:'15px' }}>
+                    <Button style={{color:'#22B4DB'}}> Edit</Button>
+                    <Button style={{color:'#DE192E'}}>Delete</Button>
+                 </Box>
+             </Box>
+
+
+
+           </Box>          
+</Container>
+
         </div>
+
     )
 }
 
